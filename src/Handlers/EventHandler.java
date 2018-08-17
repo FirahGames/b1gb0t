2fd0f9af4aa354e9d801ad9c9d2cad4b91ced668
Ghost Rider <firahgaming@gmail.com>
2018-08-17 18:59:44 -0400

    removed token, working on command handler

--- src/Handlers/EventHandler.java
+++ src/Handlers/EventHandler.java
@@ -2,7 +2,7 @@ import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-public class RespondMsg extends ListenerAdapter {
+public class EventHandler extends ListenerAdapter {
     @Override
     public void onReady(ReadyEvent event)
     {
